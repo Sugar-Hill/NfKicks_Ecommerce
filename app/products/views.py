abd89cd86f15baa57c949692488390931bf83ac7
Fouad Animashaun <ayofouad@gmail.com>
2020-07-30 14:26:46 +0100

    initial commit

---
+++ app/products/views.py
@@ -0,0 +1,70 @@
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+
+from .models import Product
+
+
+# Create your views here.
+class ProductFeaturedListView(ListView):
+    # queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.featured()
+    template_name = "products/featured-detail.html"
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     return Product.objects.featured()
+
+
+class ProductListView(ListView):
+    # queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found..")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("An error has occurred!")
+        return instance
+
+
+class ProductDetailView(DetailView):
+    # queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(
+            *args, **kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sugar_hill/Projects/ecommerce/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
